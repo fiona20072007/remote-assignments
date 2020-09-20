9b4528339878449740771b682783b0ece5247dae
Fiona <fiona20072007@gmail.com>
2020-09-20 23:32:05 +0800

    modify Assignments

--- week-1/assignment2/script.js
+++ week-1/assignment2/script.js
@@ -1,7 +1,7 @@
 //assignment 1
 
 function max(numbers) {
-  let max = -1;
+  let max = numbers[0];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > max) {
       max = numbers[i];
